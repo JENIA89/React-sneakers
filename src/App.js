8dfc8f71190458780c75d01c9458058791b111b5
Evgene Popolamov <popolamov908@gmail.com>
2021-07-29 19:37:27 +0300

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='wrapper clear'>
+      <header className='d-flex justify-between align-center p-40'>
+        <div className='d-flex align-center'>
+          <img width={40} height={40} src='/img/logo.png' />
+          <div>
+            <h3 className='text-uppercase'>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className='headerRight d-flex '>
+          <li className='mr-30 '>
+            <img width={18} src='/img/cart.png' />
+            <span>1250 руб.</span>
+          </li>
+          <li>
+            <img width={18} src='/img/user.png' />
+          </li>
+        </ul>
       </header>
+      <div className='content p-40'>
+        <h1>Все кроссовки</h1>
+      </div>
     </div>
   );
 }
