24241707b3d7703c8328c333a2745bb033ba5542
Evgene Popolamov <popolamov908@gmail.com>
2021-07-29 21:27:29 +0300

    feat: add product card

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className='headerRight d-flex '>
+        <ul className='headerRight d-flex'>
           <li className='mr-30 '>
             <img width={18} src='/img/cart.png' />
             <span>1250 руб.</span>
@@ -20,7 +20,87 @@ function App() {
         </ul>
       </header>
       <div className='content p-40'>
-        <h1>Все кроссовки</h1>
+        <h1 className='mb-40'>Все кроссовки</h1>
+        <div className='d-flex flex-wrap'>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/1.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/2.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/3.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/3.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/3.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+          <div className='card'>
+            <img width={133} height={112} src='/img/sneakers/3.jpg' alt='' />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className='d-flex justify-between align-center'>
+              <div className='d-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className='button'>
+                <img width={11} height={11} src='/img/plus.svg' alt='plus' />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
