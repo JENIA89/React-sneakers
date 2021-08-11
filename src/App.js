36b747905b2851192d0894b780e73091ca0c8c5e
Evgene Popolamov <popolamov908@gmail.com>
2021-08-11 18:37:17 +0300

    feat: add controlled input

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import axios from 'axios';
 
 function App() {
   const [items, setItems] = useState([]);
+  const [changeValue, setSearch] = useState('');
   const [isOpenedCart, setIsOpenedCart] = useState(false);
 
   useEffect(() => {
@@ -14,20 +15,41 @@ function App() {
       .then((res) => setItems(res.data));
   }, []);
 
+  const onChangeValue = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const searchValue = (arr) =>
+    arr.filter((item) =>
+      item.title.toLowerCase().includes(changeValue.toLowerCase())
+    );
+
   return (
     <div className='wrapper clear'>
       {isOpenedCart && <Drawer onClose={() => setIsOpenedCart(false)} />}
       <Header onClickCart={() => setIsOpenedCart(true)} />
       <div className='content p-40'>
         <div className='d-flex align-center justify-between mb-40'>
-          <h1>Все кроссовки</h1>
+          <h1>
+            {changeValue ? `Поиск по запросу: ${changeValue}` : 'Все кроссовки'}
+          </h1>
           <div className='search-block'>
             <img src='/img/search.png' alt='search' />
-            <input type='text' placeholder='поиск' />
+            <img
+              onClick={() => setSearch('')}
+              className='clear cu-p'
+              src='/img/btn-remove.svg'
+              alt='remove'
+            />
+            <input
+              onChange={onChangeValue}
+              placeholder='поиск'
+              value={changeValue}
+            />
           </div>
         </div>
         <div className='d-flex flex-wrap'>
-          {items.map((item, i) => (
+          {searchValue(items).map((item, i) => (
             <Card
               key={i}
               title={item.title}
